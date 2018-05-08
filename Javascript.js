088946e961adf2e3cd56b8e03ed4fa554f9a4ff2
Gabriel-Stoettinger <gabriel.stoettinger@gmx.at>
2018-05-08 16:56:07 +0200

    Cookies still not working

--- Javascript.js
+++ Javascript.js
@@ -12,7 +12,11 @@ blockbackground.src = 'block-background.jpeg';
 var solved = ctx.createPattern(blockbackground, "no-repeat");
 
 var score;
-var highscore = getCookie(highscore);
+var highscore =0;
+if (document.cookie)
+{
+    highscore=checkCookie();
+}
 var errors;
 
 var minNumber = +document.getElementById("minNum").value; //= 1;
@@ -175,25 +179,42 @@ function checkResults() {
     }
 }
 
-function setCookie(highscore) {
-    document.cookie = highscore + ";path=/";
-}
+var record = 100;
 
-function getCookie(highscore) {
-    var name = highscore+"=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
+
+
+function getCookie(c_name)
+{
+    var c_start;
+    var c_end;
+    if (document.cookie.length > 0) {
+        c_start = document.cookie.indexOf(c_name + "=");
+        if (c_start != -1) {
+            c_start = c_start + c_name.length + 1;
+            c_end = document.cookie.indexOf(";", c_start);
+            if (c_end == -1) c_end = document.cookie.length
+            return unescape(document.cookie.substring(c_start, c_end));
         }
     }
     return "";
 }
+
+
+function setCookie(c_name,value,expiredays)
+{
+    var exdate=new Date();
+    exdate.setDate(exdate.getDate()+expiredays);
+    document.cookie=c_name+ "=" +escape(value)+((expiredays==null) ? "" : "; expires="+exdate.toUTCString());
+}
+
+function checkCookie()
+{
+    recordi=getCookie('hightscore');
+}
+
+
+
+
 function main() {
 
     init();
@@ -201,7 +222,7 @@ function main() {
     checkResults();
     drawScores();
     requestAnimationFrame(main);
-    setCookie(highscore);
+    getCookie();
 }
 
 restart();
