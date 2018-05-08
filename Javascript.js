1da2e9ea4da3d3e8e2d1e19428d8f3e5b06c1159
NicoOhler <32089075+NicoOhler@users.noreply.github.com>
2018-05-08 14:27:09 +0200

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,180 @@
+    var canvas = document.getElementById("myCanvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight - 20;
+    var ctx = canvas.getContext("2d");
+
+    var countblocks = document.getElementById("countblocks").value;
+    var col = countblocks;
+    var fieldWidth = canvas.width / countblocks / 2;
+    var fieldHeight = canvas.height / countblocks / 2;
+
+    var score;
+    var highscore = 0;
+    var errors;
+
+    var minNumber=+document.getElementById("minNum").value; //= 1;
+    var maxNumber=+document.getElementById("maxNum").value;
+
+    document.addEventListener("mousedown", inputMouse, false);
+
+    var fields = [];
+
+    function init() {
+        fullscreen();
+        countblocks = document.getElementById("countblocks").value;
+        col = countblocks;
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight - 20;
+        fieldWidth = canvas.width / countblocks / 2;
+        fieldHeight = canvas.height / countblocks / 2;
+
+        /*for (rows = 0; rows < countblocks; rows++) {
+            for (columns = 0; columns < col; columns++) {
+                fields[rows][columns].x = canvas.width / 2 - (countblocks - rows) / 2 * fieldWidth + columns * fieldWidth;
+                fields[rows][columns].y = canvas.height / 2 + fieldHeight * countblocks / 3 - (fieldHeight + 2) * rows;
+            }
+            col--;
+        }*/
+    }
+
+    function restart() {
+        score = 0;
+        errors = 0;
+        col = countblocks;
+        for (rows = 0; rows < countblocks; rows++) {
+            fields[rows] = [];
+            for (columns = 0; columns < col; columns++) {
+                fields[rows][columns] = {
+                    x: canvas.width / 2 - (countblocks - rows) / 2 * (fieldWidth) + columns * fieldWidth,
+                    y: canvas.height / 2 + fieldHeight * countblocks / 3 - (fieldHeight + 2) * rows,
+                    result: 0,
+                    status: 0,
+                    input: 0
+                };
+                if (rows == 0) {
+                    fields[rows][columns].result = getRandomNumber();
+                    fields[rows][columns].input = fields[rows][columns].result;
+                    fields[rows][columns].status = 1;
+                }
+                else
+                    fields[rows][columns].result = fields[rows - 1][columns].result + fields[rows - 1][columns + 1].result;
+            }
+            col--;
+        }
+        col = countblocks;
+    }
+
+    function getRandomNumber() {
+        return (0+Math.floor((Math.random() * maxNumber))+minNumber);
+    }
+
+    //todo:
+        function fullscreen() {
+    }
+
+    function drawScores() {
+        ctx.font = "16px Verdana";
+        ctx.fillStyle = "#000000";
+        ctx.fillText("Score:\t" + score, 20, canvas.height - 75);
+
+        ctx.font = "16px Verdana";
+        ctx.fillStyle = "#000000";
+        ctx.fillText("Errors:\t" + errors, 20, canvas.height - 50);
+
+        ctx.font = "16px Verdana";
+        ctx.fillStyle = "#000000";
+        ctx.fillText("Highscore:\t" + highscore, 20, canvas.height - 25);
+    }
+    //todo: nur Zahlen eingeben
+   function inputMouse(e) {
+        var x = e.clientX - canvas.offsetLeft;
+        var y = e.clientY - canvas.offsetTop;
+
+        var input;
+        //alert(x + "  " + y);
+        col = countblocks;
+        for (rows = 0; rows < countblocks; rows++) {
+            for (columns = 0; columns < col; columns++) {
+                if (x > fields[rows][columns].x && x < fields[rows][columns].x + fieldWidth) {
+                    if (y > fields[rows][columns].y && y < fields[rows][columns].y + fieldHeight) {
+                        input=window.prompt("Geben Sie eine Zahl ein");
+                        if (input != null && input != true) {
+                            if(isNaN(input)==false)
+                            fields[rows][columns].input = Number(input);
+                            if (fields[rows][columns].input != fields[rows][columns].result) {
+                                errors++;
+                                if (score > 0)
+                                    score--;
+                                if (errors == 3) {
+                                    alert("Drei Fehler! Du hast verloren :(");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            col--;
+        }
+    }
+
+    //todo: centered - Joni
+    function drawFields() {
+        col = countblocks;
+        for (rows = 0; rows < countblocks; rows++) {
+            for (columns = 0; columns < col; columns++) {
+                ctx.beginPath();
+                ctx.rect(fields[rows][columns].x, fields[rows][columns].y, fieldWidth, fieldHeight);
+                ctx.fillStyle = "#ffa500";
+                if (fields[rows][columns].status === 1) {
+                    ctx.fillStyle = "#9acd32";
+                }
+                ctx.lineWidth = 5;
+                ctx.strokeStyle = "grey";
+                ctx.stroke();
+                ctx.fill();
+                ctx.closePath();
+
+                //todo: Zahlen nicht zentriert
+                if (fields[rows][columns].input !== 0) {
+                    ctx.font = "16px Verdana";
+                    ctx.fillStyle = "#000000";
+                    ctx.fillText(fields[rows][columns].input, fields[rows][columns].x + fieldWidth / 2 - 4, fields[rows][columns].y + fieldHeight / 2 + 8);
+                }
+            }
+            col--;
+        }
+    }
+
+    function checkResults() {
+        col = countblocks;
+        var i = 0;
+        for (rows = 0; rows < countblocks; rows++) {
+            for (columns = 0; columns < col; columns++) {
+                if (fields[rows][columns].status == 0) {
+                    i++;
+                    if (fields[rows][columns].input == fields[rows][columns].result) {
+                        fields[rows][columns].status = 1;
+                        score++;
+                        if (score > highscore)
+                            highscore = score;
+                    }
+                }
+            }
+            col--;
+        }
+        if (i == 0) {
+            alert('Sie sind ein Gewinner!!!');
+            restart();
+        }
+    }
+
+    function main() {
+        init();
+        drawFields();
+        checkResults();
+        drawScores();
+        requestAnimationFrame(main);
+    }
+
+    restart();
+    main();
